e20ae20baa8c704e155163c6e5db24f4ab4ca8b1
Tomasz Banaś <tomasz.banas123@gmail.com>
2024-10-27 14:18:48 +0100

    add auto loading of the menu scene for easier development

---
+++ Line Wars/Assets/Scripts/Utilities/BuildUtilities.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using ParrelSync;
+#endif
+
+namespace Utilities
+{
+    public static class BuildUtilities
+    {
+        public static bool IsClientBuild()
+        {
+            bool clientBuild = !Application.isBatchMode;
+
+#if UNITY_EDITOR
+            if (!ClonesManager.IsClone())
+                clientBuild = false;
+#endif
+
+            return clientBuild;
+        }
+    }
+}
---
+++ Line Wars/Assets/Scripts/Utilities/Editor/DefaultSceneLoader.cs
@@ -0,0 +1,40 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[InitializeOnLoad]
+public static class DefaultSceneLoader
+{
+    static DefaultSceneLoader()
+    {
+        EditorApplication.playModeStateChanged += LoadDefaultScene;
+    }
+
+    static void LoadDefaultScene(PlayModeStateChange state)
+    {
+        if(Utilities.BuildUtilities.IsClientBuild())
+            return;
+        
+        Debug.Log("Loading default scene...");
+        
+        if (state == PlayModeStateChange.ExitingEditMode)
+        {
+            EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+        }
+        
+        Scene currentScene = SceneManager.GetActiveScene();
+        // Scene defaultScene = SceneManager.GetSceneAt(0);
+        
+        // Check if the current scene is not the default scene
+        if (currentScene.name == "MainMenuScene") // && currentScene.path == defaultScene.path)
+            return;
+
+        if (state == PlayModeStateChange.EnteredPlayMode)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
+#endif
